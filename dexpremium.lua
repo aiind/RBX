cbf6ad9f07271d379d21d9e398bb6e1fd6b4d9cc
aiind <111066512+aiind@users.noreply.github.com>
2026-05-07 21:30:06 +0800

    Create dexpremium.lua

---
+++ dexpremium.lua
@@ -0,0 +1,4 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/jodta/my-scripts/refs/heads/main/Dex%2B%2B/Decompiler%20Fix.lua"))()
